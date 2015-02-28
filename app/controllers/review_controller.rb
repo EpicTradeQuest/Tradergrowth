6c951edd895f5ab61035bce1c5ba8f540e243629
Ben Walker <benn.walker@gmail.com>
2015-02-28 17:00:35 +1000

    Fixed tag search, added basic styling to stats(more to do), added error messages

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,22 +1,24 @@
 class ReviewController < ApplicationController
+
     def index
-        if params[:query].present?
-            @trades = Trade.tagged_with.search(params[:query])
+        if params[:query] == ''
+            flash.now[:error] = "Whoops, you didn't submit anything!  Try again?"
+        elsif params[:query].present?
+                unless Trade.tagged_with(params[:query]).exists?
+                    flash.now[:error] = "Hmmm, those tags don't seem to exist!  Try again?"
+                    render 'index'
+                else
+                    @trades = Trade.tagged_with(params[:query])
+                    @tags = params[:query]
+                    @pipresult = @trades.sum :result
+                    @average = @trades.average(:result).round(1)
+                    @winrate = ((@trades.where('result > 0').count.to_f / @trades.count.to_f) * 100).round(1)
+                    @largestwin = @trades.maximum :result
+                    @largestloss = @trades.minimum :result
+                end
         else
             @trades = Trade.all
         end
     end
-
-    def show
-        @trades = Trade.tagged_with(params[:query])
-        @tags = params[:query]
-        @pipresult = @trades.sum :result
-        @average = @trades.average(:result).round(1)
-        @winrate = ((@trades.where('result > 0').count.to_f / @trades.count.to_f) * 100).round(1)
-    end
-
-    def autocomplete
-        render json: Trade.search(params[:query], autocomplete: true)
-    end
-
 end
+
