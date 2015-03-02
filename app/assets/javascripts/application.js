255ecc6958c5284a4a4b312cf4207f65c567b0c7
Ben Walker <benn.walker@gmail.com>
2015-03-02 16:25:28 +1000

    Fixed index form, fixed tags in edit, styled edit and show pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require masonry/jquery.masonry
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree
-//= require bootstrap-sprockets
+
