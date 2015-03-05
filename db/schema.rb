dafe2beb4845fb55b6968da36844af478ca9dce9
Ben Walker <benn.walker@gmail.com>
2015-03-05 21:29:09 +1000

    Added trade-unit to registration, saves to the model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301111346) do
+ActiveRecord::Schema.define(version: 20150305102545) do
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20150301111346) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "trade_unit"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
