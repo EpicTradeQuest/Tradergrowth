a4df2e2ee4a20c031e43b04aa4d329ab9a7e84bc
Ben Walker <benn.walker@gmail.com>
2015-03-07 11:35:50 +1000

    Changes all result units based on registration selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,41 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  helper_method :unit
+
+  def unit
+    unitchoice = current_user.trade_unit
+    if unitchoice == 1
+      @unit = "pips"
+    elsif unitchoice == 2
+      @unit = "points"
+    elsif unitchoice == 3
+      @unit = "dollars"
+    elsif unitchoice == 4
+      @unit = "percent"
+    elsif unitchoice == 5
+      @unit = "R"
+    else
+      @unit = "undefined"
+    end
+  end
+
+  protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [ :email, :password, :password_confirmation, :trade_unit]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) {
+        |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) {
+        |u| u.permit(registration_params)
+      }
+    end
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305102545) do
+ActiveRecord::Schema.define(version: 20150306112255) do
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20150305102545) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "trades", force: :cascade do |t|
-    t.integer  "result"
+    t.float    "result"
     t.text     "description"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20150305102545) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.float    "trade_unit"
+    t.integer  "trade_unit"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
