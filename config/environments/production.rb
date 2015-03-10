75ddc79d5e12fd7ff03ec3cb8463a771ffa2f828
Ben Walker <benn.walker@gmail.com>
2015-03-10 21:34:22 +1000

    Mandrill take 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'sleepy-sands-6961.herokuapp.com' port: 587 }
+  config.action_mailer.default_url_options = { host: 'sleepy-sands-6961.herokuapp.com', port: 3000 }
 
   config.action_mailer.smtp_settings = {
     address:        'smtp.mandrillapp.com',
