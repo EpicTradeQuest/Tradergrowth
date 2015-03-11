a07ac8e4edb31f770147404ed8108bb60f90d205
Ben Walker <benn.walker@gmail.com>
2015-03-11 11:30:47 +1000

    testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,19 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { host: 'tradejournal.co' }
 
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.mandrillapp.com',
+    port:           587,
+    user_name:      ENV['MANDRILL_USERNAME'],
+    password:       ENV['MANDRILL_APIKEY'],
+    domain:         'tradejournal.co',
+    authentication: :plain
+  }
+  config.action_mailer.default_url_options = {
+    host: 'tradejournal.co'
+  }
 end
